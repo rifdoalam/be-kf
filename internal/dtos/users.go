63cc88aa6275870c2a5c3e17a8974eeb2fb8edd1
rifdoalam <rifdoalam@gmail.com>
2025-02-13 23:41:18 +0700

    refactor and clean code

--- internal/dtos/users.go
+++
@@ -1,7 +0,0 @@
-package dtos
-type GetUsers struct {
-	ID int `json:"id"`
-	Name string `json:"name"`
-	Email string `json:"email"`
-
-}
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"be-api/internal/handlers"
-	"be-api/internal/middlewares"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -36,21 +35,5 @@ func SetupRoutes(r *gin.Engine, db *gorm.DB) {
 
 	}
 
-	authGroup := r.Group("/api/auth")
-	{
-		authGroup.POST("/login", func(c *gin.Context) {
-			handlers.Login(c, db)
-		})
-		authGroup.POST("/register", func(c *gin.Context) {
-			handlers.Register(c, db)
-		})
-	}	
 
-	userGroup := r.Group("/api/users")
-	{
-		userGroup.Use(middlewares.JWTMiddleware)
-		userGroup.GET("/", func(c *gin.Context) {
-			handlers.GetUsers(c, db)
-		})
-	}
 }
